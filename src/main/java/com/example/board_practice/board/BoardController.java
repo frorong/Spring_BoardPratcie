4b35bb41925865f49c000831f15e28c41e69b809
Lee-Seungje <moondgod@gmail.com>
2024-05-27 23:40:11 +0900

    Add lombok

--- src/main/java/com/example/board_practice/board/BoardController.java
+++ src/main/java/com/example/board_practice/board/BoardController.java
@@ -1,11 +1,27 @@
-//package com.example.board_practice.board;
-//
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/api/v1/board")
-//public class BoardController {
-//
-//
-//}
+package com.example.board_practice.board;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/board")
+//@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardRepository boardRepository;
+
+    public BoardController(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    @GetMapping("/list")
+    List<BoardEntity> getBoardList() {
+        List<BoardEntity> responseData = boardRepository.findAll();
+
+        return responseData;
+    }
+}
