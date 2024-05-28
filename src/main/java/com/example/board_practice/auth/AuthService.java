2c9118678b6f9fb0e8cd51af07302b8818b62af5
Lee-Seungje <moondgod@gmail.com>
2024-05-28 15:16:55 +0900

    Add checkLogin

--- src/main/java/com/example/board_practice/auth/AuthService.java
+++ src/main/java/com/example/board_practice/auth/AuthService.java
@@ -3,21 +3,20 @@ package com.example.board_practice.auth;
 import com.example.board_practice.user.RequestEntity;
 import com.example.board_practice.user.UserEntity;
 import com.example.board_practice.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @Service
 public class AuthService {
     private final AuthRepository authRepository;
     private final UserRepository userRepository;
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    public AuthService(AuthRepository authRepository, UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.authRepository = authRepository;
-        this.userRepository = userRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
 
     Long saveUser(RequestEntity dto) {
         UserEntity newUser = UserEntity.builder()
@@ -28,5 +27,15 @@ public class AuthService {
         userRepository.save(newUser);
 
         return newUser.getId();
+    }
+
+    Long checkLogin(RequestEntity dto) {
+        Optional<UserEntity> targetUser = userRepository.findByEmail(dto.getEmail());
+
+        if (targetUser.isPresent() && targetUser.get().getPassword().equals(dto.getPassword())) {
+            return targetUser.get().getId();
+        }
+
+        throw new UsernameNotFoundException("User not found");
     }
 }
--- src/main/java/com/example/board_practice/user/UserService.java
+++ src/main/java/com/example/board_practice/user/UserService.java
@@ -2,31 +2,16 @@ package com.example.board_practice.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserEntity loadUserByUsername(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException((email)));
     }
-
-    Long saveUser(RequestEntity dto) {
-        UserEntity newUser = UserEntity.builder()
-                .email(dto.getEmail())
-                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
-                .build();
-
-        userRepository.save(newUser);
-
-        return newUser.getId();
-    }
-
-
 }
